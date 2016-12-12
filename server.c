251d422b4393c86b48825c64b1d595652a466de9
qjzhzw <450388261@qq.com>
2016-12-12 06:16:32 -0800

    Lab03

--- server.c
+++ server.c
@@ -123,8 +123,10 @@ void web(int fd, int hit)
 	}
 	if(fstr == 0) log(SORRY,"file extension type not supported",buffer,fd);
 
-	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
-		log(SORRY, "failed to open file",&buffer[5],fd);
+	if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {
+		log(SORRY, "404 Error",&buffer[5],fd);
+		(void)sprintf(buffer,"HTTP/1.0 404 Error\r\nContent-Type: %s\r\n\r\n", fstr);
+	}
 
 	log(LOG,"SEND",&buffer[5],hit);
 
